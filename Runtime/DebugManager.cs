0fb29340abf8a0b445bff8c888bfc8a43c0b26bb
Omar Rodríguez Pérez <omar.rodpe@gmail.com>
2020-04-02 12:30:04 -0600

    Added debug manager for controlling logs.

---
+++ Runtime/DebugManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace OhmsLibraries.Utilities {
+    public static class DebugManager {
+        [System.Diagnostics.Conditional( "ENABLE_LOGS" )]
+        public static void Log ( object s ) {
+
+            Debug.Log( s );
+
+        }
+        [System.Diagnostics.Conditional( "ENABLE_LOGS" )]
+        public static void LogFormat ( string s, params object[] format ) {
+
+            Debug.LogFormat( s, format );
+
+        }
+    }
+}
