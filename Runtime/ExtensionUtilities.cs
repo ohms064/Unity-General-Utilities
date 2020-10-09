09aae30398a16036a886c9aad961bc469689acb6
Omar Rodríguez Pérez <otokonoko064@gmail.com>
2020-10-09 00:51:40 -0500

    Added a utility for lists and arrays called PickOne.

---
+++ Runtime/ExtensionUtilities.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OhmsLibraries.Utilities.Extensions {
+    public static class ExtensionUtilities {
+        public static T PickOne<T>( this List<T> list ) {
+            return list[Random.Range( 0, list.Count )];
+        }
+
+        public static T PickOne<T>( this T[] array ) {
+            return array[Random.Range( 0, array.Length )];
+        }
+    }
+}
